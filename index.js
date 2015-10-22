bd13e75225e73c7d80fe6361893d77edacd66369
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2015-10-22 15:52:55 +0200

    initial new frontend commit

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
 #!/usr/bin/env node
-var app=require("express")(),
-    http=require("http").Server(app),
-    fs=require("fs"),
-    io=require('./io.js')(http);
+var app = require("express")(),
+    http = require("http").Server(app),
+    fs = require("fs"),
+    io = require('./io.js')(http);
 
-var HTTPPORT=3456;
+var HTTPPORT = 3456;
+var COUNTDOWNTIME = 3000;
 
-var COUNTDOWNTIME=3000;
+//var SOURCEFILE = process.argv[1];
+var SOURCEFILE = "mariolang.cpp";
 
-//var SOURCEFILE=process.argv[1];
-var SOURCEFILE="mariolang.cpp";
+var conntab = {},
+	nicklist = [];
 
-
-var conntab={},nicklist=[];
-
-
-function getTypeText(){
-	var lines=String(fs.readFileSync(SOURCEFILE)).replace(/\n+/g,"\n").split("\n");
-	var start=~~(Math.random()*(lines.length-10));
-	var res="";
+function getTypeText () {
+	var lines = String(fs.readFileSync(SOURCEFILE)).replace(/\n+/g,"\n").split("\n");
+	var start = ~~(Math.random()*(lines.length-10));
+	var res = "";
 	var i;
-	for(i=0;i<10&&res.length<120;i++)res+=lines[start+i]+" ";
+	for(i = 0; i < 10 && res.length < 120; i++)
+		res += lines[start+i] + " ";
 	return res.replace(/[ \t]+/g," ").trim();
 }
 
-
-app.get("/",function(req,res){
-	res.sendFile(__dirname+"/index.html");
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/client/index.html');
 });
 
+app.get('/bundle.js', function (req, res) {
+	res.sendFile(__dirname + '/client/bundle.js');
+});
+
+app.get('/css/:file', function (req, res) {
+	res.sendFile(__dirname + '/client/css/' + req.params.file);
+});
 
 http.listen(HTTPPORT,function(){
 	console.log("Listening on http://localhost:"+HTTPPORT);
